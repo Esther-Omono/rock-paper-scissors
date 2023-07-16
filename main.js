3ab2c870bb7af1c52d614c70548bbede480c497d
Esther-Omono <enitanatakere@gmail.com>
2023-07-16 17:47:51 +0100

    Made few changes! Added a test to show the result of the playGame function on the console.

--- main.js
+++ main.js
@@ -9,17 +9,23 @@ console.log("Computer played: " + getComputerChoice());
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return "It's a Tie!"
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock breaks Scissors!"
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper covers Rock"
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! Scissors cuts Paper"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return `You lose! ${computerSelection} covers ${playerSelection}`
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `You lose! ${computerSelection} cuts ${playerSelection}`
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `You lose! ${computerSelection} breaks ${playerSelection}`
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+        return `Select one of the options!`
     }
 }
 
-const playerSelection = "rock";
+const playerSelection = prompt("Rock, Paper or Scissors? Pick One!").toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));

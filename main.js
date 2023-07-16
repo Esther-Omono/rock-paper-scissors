68e5ab0140d504a9cddd389916c24578676db248
Esther-Omono <enitanatakere@gmail.com>
2023-07-16 14:17:23 +0100

    Added a function to get the User's choice

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Function to get the Computer's choice"
+
 function getComputerChoice() {
     const randomChoice = (Math.floor(Math.random() * 3) + 1);
     if (randomChoice === 1) {
@@ -9,3 +11,18 @@ function getComputerChoice() {
     }
 }
 console.log("Computer played: " + getComputerChoice());
+
+// Function to get the User's choice
+const getUserChoice = (userInput) => {
+    userInput = prompt();
+    if (userInput.toLowerCase() === "rock" ) {
+      return userInput;
+    } else if (userInput.toLowerCase() === "paper") {
+      return userInput;
+    } else if (userInput.toLowerCase() === "scissors") {
+      return userInput;
+    } else {
+      alert("Invalid Option!!!");
+    }
+  }
+  console.log("User played: " + getUserChoice())

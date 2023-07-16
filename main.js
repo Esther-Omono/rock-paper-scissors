c1e4ca3ea623575c9aa0258452726c04861e6b7c
Esther-Omono <enitanatakere@gmail.com>
2023-07-16 17:12:23 +0100

    Added a function that plays a single round of the game

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ function getComputerChoice() {
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
 console.log("Computer played: " + getComputerChoice());
+
+// Function that plays a single round of the game
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a Tie!"
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock breaks Scissors!"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper covers Rock"
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors cuts Paper"
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}

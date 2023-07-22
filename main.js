029fb72749861ac3b60d8587b7281612a3e1219e
Esther-Omono <enitanatakere@gmail.com>
2023-07-22 07:09:49 +0100

    changed the named functions to arrow functions

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Function to get the Computer's choice"
-function getComputerChoice() {
+const getComputerChoice = () => {
     const randomChoice = ["rock", "paper", "scissors"];
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
 console.log("Computer played: " + getComputerChoice());
 
 // Function that plays a single round of the game
-function playRound(playerSelection, computerSelection) {
+const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
         return "It's a Tie!"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
@@ -29,3 +29,4 @@ function playRound(playerSelection, computerSelection) {
 const playerSelection = prompt("Rock, Paper or Scissors? Pick One!").toLowerCase();
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+

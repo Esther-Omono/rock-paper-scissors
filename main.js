b859b63d0093890cf9a5707c7ef23a8951de6c93
Esther-Omono <enitanatakere@gmail.com>
2023-07-22 08:10:10 +0100

    added a function to play 5 rounds and keep track of scores

--- main.js
+++ main.js
@@ -1,32 +1,58 @@
+// Score variables
+let playerScore = 0;
+let compScore = 0;
+
 // Function to get the Computer's choice"
 const getComputerChoice = () => {
     const randomChoice = ["rock", "paper", "scissors"];
     return randomChoice[Math.floor(Math.random() * randomChoice.length)];
 }
-console.log("Computer played: " + getComputerChoice());
 
 // Function that plays a single round of the game
 const playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
-        return "It's a Tie!"
+        return "It's a Tie!";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock breaks Scissors!"
+        playerScore++;
+        return "You win! Rock breaks Scissors!";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper covers Rock"
+        playerScore++;
+        return "You win! Paper covers Rock";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors cuts Paper"
+        playerScore++;
+        return "You win! Scissors cuts Paper";
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return `You lose! ${computerSelection} covers ${playerSelection}`
+        compScore++;
+        return `You lose! ${computerSelection} covers ${playerSelection}`;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return `You lose! ${computerSelection} cuts ${playerSelection}`
+        compScore++;
+        return `You lose! ${computerSelection} cuts ${playerSelection}`;
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return `You lose! ${computerSelection} breaks ${playerSelection}`
+        compScore++;
+        return `You lose! ${computerSelection} breaks ${playerSelection}`;
     } else {
-        return `Select one of the options!`
+        return "Select one of the options!";
     }
 }
 
-const playerSelection = prompt("Rock, Paper or Scissors? Pick One!").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+/*
+Function that plays 5 rounds of the game and keeps scores to 
+report a winner or loser at the end
+*/
+const game = () => {
+    for(let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock, Paper or Scissors? Pick One!").toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
 
+    if (playerScore > compScore) {
+        return "You beat the computer! Hurray!";
+    } else if (playerScore < compScore) {
+        return "Arrgh, You lost to the computer! Sad!";
+    } else {
+        return "You tied with the computer!";
+    }
+}
+
+console.log(game());
